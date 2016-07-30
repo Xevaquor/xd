b67ce2dc5e004112f11fc497c2051a5cd5ba88ea
Xevaquor <xevaquor@hotmail.com>
2016-07-31 00:56:02 +0200

    dfeff

--- preproces.py
+++ preproces.py
@@ -6,6 +6,7 @@ import nltk
 import langid
 import pymongo
 import numpy as np
+import pickle
 
 mongo = pymongo.MongoClient()
 
@@ -23,6 +24,7 @@ setoftokens = sorted(list(set(listoftokens)))
 tokens_to_idx = dict((c, i) for i, c in enumerate(setoftokens))
 n_tokens = len(listoftokens)
 n_vocab = len(setoftokens)
+idx_to_tokens = dict((i, c) for i, c in enumerate(setoftokens))
 
 print n_tokens, ' tokens'
 print n_vocab, ' vocabulary size'
@@ -59,3 +61,21 @@ y = to_categorical(dataY)
 
 np.save('x', X)
 np.save('y', y)
+
+with open('setoftokens.pickle', 'wb') as handle:
+  pickle.dump(setoftokens, handle)
+with open('n_tokens.pickle', 'wb') as handle:
+  pickle.dump(n_tokens, handle)
+with open('tokens_to_idx.pickle', 'wb') as handle:
+  pickle.dump(tokens_to_idx, handle)
+with open('n_vocab.pickle', 'wb') as handle:
+  pickle.dump(n_vocab, handle)
+with open('idx_to_tokens.pickle', 'wb') as handle:
+  pickle.dump(idx_to_tokens, handle)
+
+
+setoftokens = sorted(list(set(listoftokens)))
+tokens_to_idx = dict((c, i) for i, c in enumerate(setoftokens))
+n_tokens = len(listoftokens)
+n_vocab = len(setoftokens)
+idx_to_tokens = dict((i, c) for i, c in enumerate(setoftokens))
