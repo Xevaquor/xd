4b81de280007fe1752fe7115b122957424dfa99e
Mateusz Bednarski <mateusz.bednarski@roche.com>
2016-08-01 13:34:19 +0200

    updated

--- grab.py
+++ grab.py
@@ -1,20 +1,16 @@
 # coding=utf-8
-
 import requests
 from pprint import pprint
 import nltk
 import langid
-import pymongo
+import codecs
+from tqdm import tqdm
 
-mongo = pymongo.MongoClient()
+output_file = 'romantyzm.txt'
 
-db = mongo.mickiewicz
+endpoint = 'http://wolnelektury.pl/api/epochs/romantyzm/books/'
 
-books = db.books
-
-mickiewicz = 'http://wolnelektury.pl/api/epochs/romantyzm/books/'
-
-response = requests.get(mickiewicz)
+response = requests.get(endpoint)
 
 contents = [x['href'] for x in response.json()]
 pprint(contents)
@@ -22,15 +18,14 @@ allbooks = len(contents)
 print "Found books: ", allbooks
 
 def extract_text(url):
-    print "Extracting from ", url
 
     raw = requests.get(url).text
 
     lang = langid.classify(raw)[0]
-    print lang
+    #print lang
     if lang != 'pl' or raw.startswith('Adomas'):
-        print 'Not polish, omitting'
-        return []
+        #print 'Not polish, omitting'
+        return ''
 
     idx = raw.find(u'-----\r\nTa lektura, podobnie jak tysiące ')
     startidx = raw.find('\r\n'*3)
@@ -39,33 +34,18 @@ def extract_text(url):
     meat = raw[startidx+6:idx]
 
     meat = meat.replace(u'\r\n', '\n')
+    return meat
 
-    books.insert({'text': meat})
-
-    return nltk.word_tokenize(meat)
-
-listoftokens = []
 counter = 1
 
-for c in contents:
-    print counter, '/', allbooks
-    counter += 1
-    r = requests.get(c).json()
-    txturl = r['txt']
-    if txturl == '':
-        continue
-    pprint(txturl)
-    txt = requests.get(txturl)
-    listoftokens += extract_text(txturl)
-
-
-setoftokens = sorted(list(set(listoftokens)))
-tokens_to_idx = dict((c, i) for i, c in enumerate(setoftokens))
-n_tokens = len(listoftokens)
-n_vocab = len(setoftokens)
-
-print n_tokens, ' tokens'
-print n_vocab, ' vocabulary size'
-
-
-
+with codecs.open(output_file, 'w', encoding='utf-8') as f:
+    for c in tqdm(contents):
+        #print counter, '/', allbooks
+        counter += 1
+        r = requests.get(c).json()
+        txturl = r['txt']
+        if txturl == '':
+            continue
+        tqdm.write(txturl)
+        txt = requests.get(txturl)
+        f.write(extract_text(txturl))
